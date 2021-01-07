088406d63dba77713437f67ec420ff6907c64594
wholee@smilegate.com <wholee@smilegate.com>
2021-01-07 21:41:57 +0900

    animation enterNewScene Update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     let yOffset = 0; // window.pageYOffset 대신 쓸 변수
     let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보자 이전에 위치한 스크롤 섹션들의 스트롤 높이값의 합
     let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    let enterNewScene = false; // 새로운 scene이 시작된 순간 true
 
     const sceneInfo = [
         {
@@ -84,14 +85,17 @@
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         const currentYOffset = yOffset - prevScrollHeight;
+
+        console.log(currentScene);
     
         switch (currentScene) {
             case 0:                
                 //console.log('0 play');
                 let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
                 objs.messageA.style.opacity = messageA_opacity_in;
+                console.log(messageA_opacity_in);
                 break;
-                
+
             case 1:
                 // console.log('1 play');
                 break;
@@ -107,21 +111,26 @@
     }
 
     function scrollLoop() {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`); 
         }
 
         if (yOffset < prevScrollHeight) {
+            enterNewScene = true;
             if (currentScene === 0) retuen; // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`); 
         }
+
+        if(enterNewScene) return;
 
         playAnimation();
     }
