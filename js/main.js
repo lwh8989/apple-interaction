a6f34acee65731abe09c589a19b062504ec31eb2
wholee@smilegate.com <wholee@smilegate.com>
2021-01-14 23:46:20 +0900

    비디오 인터렉션 적용

--- js/main.js
+++ js/main.js
@@ -16,9 +16,14 @@
                 messageA: document.querySelector('#scroll-section-0 .main-message.active-0-a'),
                 messageB: document.querySelector('#scroll-section-0 .main-message.active-0-b'),
                 messageC: document.querySelector('#scroll-section-0 .main-message.active-0-c'),
-                messageD: document.querySelector('#scroll-section-0 .main-message.active-0-d')
+                messageD: document.querySelector('#scroll-section-0 .main-message.active-0-d'),
+                canvas: document.querySelector('#video-canvas-0'),
+				context: document.querySelector('#video-canvas-0').getContext('2d'),
+				videoImages: []
             },
             values: {
+                videoImageCount: 300,
+                imageSequence: [0, 299],
                 messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
                 messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
                 messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -89,6 +94,15 @@
         }
     ];
 
+    function setCanvasImages() {
+        for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+            imgElem = new Image();
+            imgElem.src = `./video/001/IMG_${6726 + i}.jpg`;
+            sceneInfo[0].objs.videoImages.push(imgElem);
+        }
+    }
+    setCanvasImages();
+
     function setLayout() {
         for (let i = 0; i < sceneInfo.length; i++ ) {
             if (sceneInfo[i].type === 'sticky') {
@@ -150,6 +164,9 @@
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
+                let sequence = Math.round(calcValues(values.imageSequence, currentYOffset));
+                objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+
                 if (scrollRatio <= 0.22) {
                     // in
                     objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
